3ca3dde05d759f0c53c98e40c9e8c828cb61d8ca
George Richardson <sonotude@gmail.com>
2015-03-06 14:49:06 +0000

    Changed orientation switcher to a more robust method

--- src/AstroWeather/Main.java
+++ src/AstroWeather/Main.java
@@ -1,6 +1,8 @@
 package AstroWeather;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,22 +10,25 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import Common.AstroPanel;
+import Common.Resources;
+import Home.MainPanel;
 
 @SuppressWarnings("serial")
 public class Main extends JFrame {
 	
 	private boolean orientation;
 	private AstroPanel p;
-	private static final int lh = 393, lw = 498, ph = 553, pw = 338;
+	private static final Dimension landscapeDimension = new Dimension(480, 320), portraitDimension = new Dimension(320, 480);
 
 	public static void main(String[] args) {
 		new Main();
 	}
 	
 	private Main() {
-		setSize(pw, ph);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(Resources.homeButtonFont);
 		orientation = true;
-		setLocationRelativeTo(null);
+		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		JButton switchOrientationBtn = new JButton("Switch Orientation");
@@ -36,45 +41,33 @@ public class Main extends JFrame {
 		
 		add(switchOrientationBtn, BorderLayout.SOUTH);
 		p = new MainPanel(this, orientation);
-		add(p);
-		validate();
+		p.setMinimumSize(new Dimension(320, 480));
+		p.setPreferredSize(new Dimension(320, 480));
+		p.setMaximumSize(new Dimension(320, 480));
+		add(p, BorderLayout.CENTER);
+		pack();
+		setLocationRelativeTo(null);
 		setVisible(true);
-		printSize();
-		
 	}
 
 	
 	private void switchOrientation() {
-		if (orientation) setSize(lw, lh);
-		else setSize(pw ,ph);
+		if (orientation) changeDimensions(landscapeDimension);
+		else changeDimensions(portraitDimension);
 		orientation = !orientation;
-		validate();
-		printSize();
-		
+		p.changeOrientation(orientation);
+		pack();		
+	}
+	
+	private void changeDimensions(Dimension d) {
+		p.setMinimumSize(d);
+		p.setPreferredSize(d);
+		p.setMaximumSize(d);
 	}
 	
 	public void changePanel(AstroPanel p) {
 		this.remove(this.p);
 		add(p, BorderLayout.CENTER);
-	}
-
-	// Used to check if the panel is the correct size.
-	private void printSize() {
-		System.out.println();
-		int h = p.getSize().height;
-		int w = p.getSize().width;
-		int optimalh, optimalw;
-		if (!orientation) System.out.println("Landscape");
-		else System.out.println("Portrait");
-		System.out.println("h" + h + " w" + w);
-		if (!orientation) {
-			optimalh = 320 + Math.abs(320 - h);
-			optimalw = 480 + Math.abs(480 - w);
-		} else {
-			optimalh = 480 + Math.abs(480 - h);
-			optimalw = 320 + Math.abs(320 - w);
-		}
-		System.out.println("oh" + optimalh + " ow" + optimalw);
 	}
 
 }
