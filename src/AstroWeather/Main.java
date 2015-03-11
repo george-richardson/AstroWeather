403643db27145a4b6ea3a362bc3e0faf41a90c4a
George Richardson <sonotude@gmail.com>
2015-03-11 14:09:17 +0000

    removed old files

--- src/AstroWeather/Main.java
+++ src/AstroWeather/Main.java
@@ -1,13 +1,8 @@
 package AstroWeather;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
-import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
