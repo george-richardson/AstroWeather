ec28817414aeb078cf7441c098a047ac59f6d344
Beneen Tahir <beneen3012@msn.com>
2015-03-12 13:04:56 +0000

    added back button to graph panel

--- src/Graphs/WeatherGraphPanel.java
+++ src/Graphs/WeatherGraphPanel.java
@@ -4,16 +4,36 @@ import API.Forecast;
 import API.Forecasts;
 import AstroWeather.Main;
 import Common.AstroPanel;
+import Home.MainPanel;
+
 import org.jfree.data.xy.XYSeries;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.JButton;
+
 @SuppressWarnings("serial")
+
+	 
 public abstract class WeatherGraphPanel extends AstroPanel {
+	
+	private ActionListener backListener = new ActionListener() {
+
+		  @Override
+		  public void actionPerformed(ActionEvent e) {
+		   parent.changePanel(new MainPanel(parent, orientation, forecasts));
+		  }
+	};
+
 
 	public WeatherGraphPanel(Main parent, boolean orientation, Forecasts forecasts, String graphTitle) {
 		super(parent, orientation, forecasts);
-
+		JButton back = new JButton("Back");
+		back.addActionListener(backListener);
+		add(back, BorderLayout.NORTH);
 		setLayout(new BorderLayout());
 		add(new WeatherGraph(graphTitle, collectDataByHour(forecasts.getHourlyForecasts(), graphTitle)), BorderLayout.CENTER);
 	}
